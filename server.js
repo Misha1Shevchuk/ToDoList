d037c1d497ab4d8bcb9cad85b286cf38398d6014
Misha1Shevhuk <misha1shevchuk@gmail.com>
2019-07-31 21:47:30 +0300

    ejs was changed on pug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
  var express = require('express');
  var app = express();
  app.use('/public', express.static('public'));
- app.set('view engine', 'ejs');
+ app.set('view engine', 'pug');
 
  app.get('/', function(req, res) {
      res.render("index");
@@ -10,7 +10,7 @@
  var routes = require("./route");
  routes.configure(app);
 
- // listening application on port 8080  
- var server = app.listen(8080, function() {
+ // listening application on port 3000  
+ var server = app.listen(3000, function() {
      console.log('Server Listening on port ' + server.address().port);
  });
