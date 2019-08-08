1235e835d7d30ac0b396e9e19503dab27d679d5b
Misha1Shevhuk <misha1shevchuk@gmail.com>
2019-08-08 20:11:11 +0300

    fixed bugs

--- client/src/components/Tasks/ItemTask.js
+++ client/src/components/Tasks/ItemTask.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 const ItemTask = props => (
-    <li className="tasks-class" id={"task-id-" + props.list[id].id_task}>
-        <input type="checkbox" checked id={"checkbox-number-" + props.list[id].id_task} className="check" />
-        <span className="task"> {props.list[id].task} </span>
-        <b id={"three-points-button-task-" + props.list[id].id_task} className="three-points-button">···</b>
-        <div className="buttons-block" id={"buttons-block-task-" + props.list[id].id_task}>
-            <b id={"change-task-button-" + props.list[id].id_task} className="item-change-button">🖉 Редагувати</b>
-            <b id={"remove-task-button-" + props.list[id].id_task} className="item-delete-button">X Видалити</b>
-        </div>
-    </li>
+    // <li className="tasks-class" id={"task-id-" + props.list[id].id_task}>
+    //     <input type="checkbox" checked id={"checkbox-number-" + props.list[id].id_task} className="check" />
+    //     <span className="task"> {props.list[id].task} </span>
+    //     <b id={"three-points-button-task-" + props.list[id].id_task} className="three-points-button">···</b>
+    //     <div className="buttons-block" id={"buttons-block-task-" + props.list[id].id_task}>
+    //         <b id={"change-task-button-" + props.list[id].id_task} className="item-change-button">🖉 Редагувати</b>
+    //         <b id={"remove-task-button-" + props.list[id].id_task} className="item-delete-button">X Видалити</b>
+    //     </div>
+    // </li>
+    <li>{props.list}</li>
 );
 
 export default ItemTask;
