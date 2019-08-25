764a6cd300af5afbbbc9d22ea3da3d5729d8cc15
Mykhailo1Shevchuk <misha1shevchuk@gmail.com>
2019-08-25 18:57:27 +0300

    fixed bug

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
  const express = require('express');
  const app = express();
+ const path = require('path');
 
  const controller = require("./controller");
  controller.configure(app);
 
- // listening application on port 3001 
- let server = app.listen(3001, () => {
+ // serve static files from the React app
+ app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+ app.get('*', (req, res) => {
+     res.sendFile(path.join(__dirname + '/client/build/index.html'));
+ });
+
+ // listening application on port
+ let server = app.listen(process.env.PORT || 3001, () => {
      console.log(`Server Listening on port ${server.address().port}`);
  });
