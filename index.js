d231747b9484e3a881d8a784c57030b2d21f3744
Misha1Shevhuk <misha1shevchuk@gmail.com>
2020-10-30 21:13:15 +0200

    remove useless comments

--- index.js
+++ index.js
@@ -7,26 +7,22 @@ const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-// Import Routes
 const auth = require("./routes/auth");
 const tasks = require("./routes/tasks");
 const projects = require("./routes/projects");
 
-//Middleware
 app.use(express.json());
 app.use(cors());
-// Route Middlevares
+
 app.use("/api/user", auth);
 app.use("/api/tasks", tasks);
 app.use("/api/projects", projects);
 
-// Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-// listening application on port
 let server = app.listen(process.env.PORT || 5000, () => {
     console.log(`Server Listening on port ${server.address().port}`);
 });
